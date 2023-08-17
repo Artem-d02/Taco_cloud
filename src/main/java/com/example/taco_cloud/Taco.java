e2ec5498d9dda84ef4acf0d8ba7070613b407311
Artem-d02 <artemd2002@yandex.ru>
2023-08-17 16:11:47 +0300

    Added annotations for checking blank filling errors

--- src/main/java/com/example/taco_cloud/Taco.java
+++ src/main/java/com/example/taco_cloud/Taco.java
@@ -1,11 +1,17 @@
 package com.example.taco_cloud;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 import java.util.List;
 
 @Data
 public class Taco {
+    @NotNull
+    @Size(min = 5, message = "Name should be longer then 5 symbols")
     private String name;
+
+    @NotNull
+    @Size(min = 1, message = "Taco should contain at least 1 ingredient")
     private List<Ingredient> ingredients;
 }
