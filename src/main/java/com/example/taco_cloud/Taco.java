24c7b811f350eeea6cefaf11187d5b7da7c6b1f6
Artem-d02 <artemd2002@yandex.ru>
2023-08-24 18:19:21 +0300

    Refactored: work with database using Spring Data JDBC replaced by another one with Spring Data JPA

--- src/main/java/com/example/taco_cloud/Taco.java
+++ src/main/java/com/example/taco_cloud/Taco.java
@@ -1,16 +1,15 @@
 package com.example.taco_cloud;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Taco {
     @NotNull
     @Size(min = 5, message = "Name should be longer then 5 symbols")
@@ -18,9 +17,11 @@ public class Taco {
 
     @NotNull
     @Size(min = 1, message = "Taco should contain at least 1 ingredient")
+    @ManyToMany
     private List<Ingredient> ingredients;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date createdAt = new Date();
 }
--- src/main/java/com/example/taco_cloud/data/IngredientRepository.java
+++ src/main/java/com/example/taco_cloud/data/IngredientRepository.java
@@ -1,9 +1,6 @@
 package com.example.taco_cloud.data;
 
 import com.example.taco_cloud.Ingredient;
-import org.springframework.data.relational.core.sql.In;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, String> {}
